2cde81f32f9be06298f9c94392c1c93ab8b86106
nouuhaa <108287720+nouuhaa@users.noreply.github.com>
2022-07-17 23:08:23 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,30 @@
+const products = [
+  {
+    id: 0,
+    name: "T-shirt 1",
+    price: 29.99,
+    instock: 100,
+    description:
+      "YELLOW",
+    imgSrc: "./img/t1.png",
+  },
+  {
+    id: 1,
+    name: "T-shirt 2",
+    price: 24.99,
+    instock: 43,
+    description:
+      "GREEN",
+    imgSrc: "./img/t2.png",
+  },
+  {
+    id: 2,
+    name: "T-shirt 3",
+    price: 19.99,
+    instock: 10,
+    description:
+      "RED",
+    imgSrc: "./img/t3.png",
+  },
+ 
+];
